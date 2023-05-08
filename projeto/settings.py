07c61c5572561f2e49bd3aaa7ecc29473b51d03c
Stoski_Ubunto <cristiano.stoski@gmail.com>
2023-05-07 23:30:50 -0300

    Criado App Produto

--- projeto/settings.py
+++ projeto/settings.py
@@ -39,8 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # myapps
     'projeto.core',
+    'projeto.produto',
 ]
 
 MIDDLEWARE = [
